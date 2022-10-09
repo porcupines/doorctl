7df688a9c906c93d299e24ac7407fb3651ba0a9b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-10-09 19:38:58 -0400

    fetch nfc keys api

--- src/Api.hs
+++ src/Api.hs
@@ -3,15 +3,16 @@
 
 module Api
   ( logAccessAttempt
+  , fetchNFCKeys
   ) where
 
 
-import Config (Config, doorLogUrl)
+import Config (Config (doorLogUrl, doorNFCUrl))
 import Data.Maybe (fromMaybe)
 import Data.Proxy (Proxy (Proxy))
 import Data.Text (unpack)
 import Data.Time.Clock (UTCTime)
-import DoorctlAPI (LogAccessAttemptAPI, NFCKey (..), AccessAttemptResult (..))
+import DoorctlAPI (LogAccessAttemptAPI, NFCKey (..), AccessAttemptResult (..), NFCKeys (..), Signature, FetchNFCKeysAPI)
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Servant.Client (runClientM, client, ClientEnv, mkClientEnv, parseBaseUrl)
 
@@ -41,3 +42,27 @@ accessAttemptClientEnv cfg = do
   pure . mkClientEnv mgr 
     . fromMaybe (error ("malformed doorLogUrl"))
     $ parseBaseUrl (unpack (doorLogUrl cfg))
+
+
+fetchNFCKeys :: Config -> UTCTime -> Signature -> IO NFCKeys
+fetchNFCKeys cfg time signature = do
+  env <- fetchNFCKeysClientEnv cfg
+  res <- runClientM
+    (client (Proxy @FetchNFCKeysAPI)
+      time signature)
+    env
+  case res of
+    Right keys -> pure keys
+    Left err -> do
+      putStrLn $
+        "error fetching NFC keys: "
+        <> show err
+      pure (NFCKeys [])
+
+
+fetchNFCKeysClientEnv :: Config -> IO ClientEnv
+fetchNFCKeysClientEnv cfg = do
+  mgr <- newManager defaultManagerSettings
+  pure . mkClientEnv mgr
+    . fromMaybe (error "malformed doorNFCUrl")
+    $ parseBaseUrl (unpack (doorNFCUrl cfg))
