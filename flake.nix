0f5f16e4bada58a8f09b0b3ce26da4c3317cf9cb
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-10-09 20:51:40 -0400

    compile using musl

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             });
           };
 
-        pkgs = nixpkgs.legacyPackages.${system}.appendOverlays([ overlay ]);
+        pkgs = nixpkgs.legacyPackages.${system}.pkgsMusl.appendOverlays([ overlay ]);
 
         haskellPackages = pkgs.haskellPackages;
 
